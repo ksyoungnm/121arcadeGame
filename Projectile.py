91d7a6dd741fae977a17bdd4c31f4bdc5ff1f559
ksyoungnm <38058671+ksyoungnm@users.noreply.github.com>
2018-04-17 17:21:23 -0700

    Add files via upload

--- Projectile.py
+++ Projectile.py
@@ -1,79 +1,68 @@
 from Agent import Agent
 from geometry import Vector2D, Point2D
 
-class Launcher(Agent):
+# class Launcher(Agent):
 
-    def __init__(self,position,direction,world):
-        super().__init__(position,world)
-        self.direction = direction
+#     def __init__(self,position,direction,world):
+#         super().__init__(position,world)
+#         self.direction = direction
 
-    def shape(self):
-        dirc = self.direction
-        perp = self.direction.perp()
-        p1 = self.position + -dirc.over(1.5) + perp.over(3.0)
-        p2 = self.position + dirc.over(4.0) + perp.over(3.0)
-        p3 = self.position + dirc.over(4.0) + perp.over(7.0)
-        p4 = self.position + dirc.over(1.0) + perp.over(7.0)
-        p5 = self.position + dirc.over(1.0) + -perp.over(7.0)
-        p6 = self.position + dirc.over(4.0) + -perp.over(7.0)
-        p7 = self.position + dirc.over(4.0) + -perp.over(3.0)
-        p8 = self.position + -dirc.over(1.5) + -perp.over(3.0)
-        return[p1,p2,p3,p4,p5,p6,p7,p8]
+#     def shape(self):
+#         dirc = self.direction
+#         perp = self.direction.perp()
+#         p1 = self.position + -dirc.over(1.5) + perp.over(3.0)
+#         p2 = self.position + dirc.over(4.0) + perp.over(3.0)
+#         p3 = self.position + dirc.over(4.0) + perp.over(7.0)
+#         p4 = self.position + dirc.over(1.0) + perp.over(7.0)
+#         p5 = self.position + dirc.over(1.0) + -perp.over(7.0)
+#         p6 = self.position + dirc.over(4.0) + -perp.over(7.0)
+#         p7 = self.position + dirc.over(4.0) + -perp.over(3.0)
+#         p8 = self.position + -dirc.over(1.5) + -perp.over(3.0)
+#         return[p1,p2,p3,p4,p5,p6,p7,p8]
 
-    def color(self):
-        return '#9494b8'
+#     def color(self):
+#         return '#9494b8'
 
-    def update(self):
-        heading = self.world.character.position - self.position
-        self.direction = heading.direction()
+#     def update(self):
+#         heading = self.world.character.position - self.position
+#         self.direction = heading.direction()
 
-    def fire(self):
-        Projectile(self.position,self.direction,self.world)
+#     def fire(self):
+#         Projectile(self.position,self.direction,self.world)
 
-    def shapeOLD(self):
-        p1 = self.position + Vector2D(-0.35,-1.0)
-        p2 = self.position + Vector2D(-0.35,0.5)
-        p3 = self.position + Vector2D(-0.15,0.5)
-        p4 = self.position + Vector2D(-0.15,1.5)
-        p5 = self.position + Vector2D(0.15,1.5)
-        p6 = self.position + Vector2D(0.15,0.5)
-        p7 = self.position + Vector2D(0.35,0.5)
-        p8 = self.position + Vector2D(0.35,-1.0)
-        return[p1,p2,p3,p4,p5,p6,p7,p8]
+# class Projectile(Agent):
 
-class Projectile(Agent):
-
-    MAX_SPEED = 0.5
+#     MAX_SPEED = 0.5
 
 
-    def __init__(self,position,velocity,world):
-        super().__init__(position,world)
-        self.velocity = velocity
-        self.shapekind = 'oval'
-        self.size = 0.6
-        self.v0 = self.velocity.over(2.0) + self.velocity.perp().over(4.0)
-        self.v1 = self.velocity.over(2.0) + -self.velocity.perp().over(4.0)
-        self.v2 = -self.velocity.over(2.0) + -self.velocity.perp().over(4.0)
-        self.v3 = -self.velocity.over(2.0) + self.velocity.perp().over(4.0)
-        self.hit_radius = 0.4
+#     def __init__(self,position,velocity,world):
+#         super().__init__(position,world)
+#         self.velocity = velocity
+#         self.shapekind = 'oval'
+#         self.size = 0.6
+#         self.v0 = self.velocity.over(2.0) + self.velocity.perp().over(4.0)
+#         self.v1 = self.velocity.over(2.0) + -self.velocity.perp().over(4.0)
+#         self.v2 = -self.velocity.over(2.0) + -self.velocity.perp().over(4.0)
+#         self.v3 = -self.velocity.over(2.0) + self.velocity.perp().over(4.0)
+#         self.hit_radius = 0.4
 
-    def shape(self):
-        p0 = self.position + self.v0
-        p1 = self.position + self.v1
-        p2 = self.position + self.v2
-        p3 = self.position + self.v3
-        return [p0,p1,p2,p3]
+#     def shape(self):
+#         p0 = self.position + self.v0
+#         p1 = self.position + self.v1
+#         p2 = self.position + self.v2
+#         p3 = self.position + self.v3
+#         return [p0,p1,p2,p3]
 
-    def color(self):
-        return "#FF0000"
+#     def color(self):
+#         return "#FF0000"
 
-    def update(self):
-        self.position = self.position + (self.velocity * self.MAX_SPEED)
-        leading_edge = self.position + self.velocity.over(4.0)
-        if (leading_edge.x > self.world.wallbounds.xmax) or (leading_edge.x < self.world.wallbounds.xmin) or (leading_edge.y > self.world.wallbounds.ymax) or (leading_edge.y < self.world.wallbounds.ymin):
-            self.world.remove(self)
-        if (self.world.character.position-self.position).magnitude() - self.world.character.hit_radius < self.hit_radius:
-            self.world.gameover = True
+#     def update(self):
+#         self.position = self.position + (self.velocity * self.MAX_SPEED)
+#         leading_edge = self.position + self.velocity.over(4.0)
+#         if (leading_edge.x > self.world.wallbounds.xmax) or (leading_edge.x < self.world.wallbounds.xmin) or (leading_edge.y > self.world.wallbounds.ymax) or (leading_edge.y < self.world.wallbounds.ymin):
+#             self.world.remove(self)
+#         if (self.world.character.position-self.position).magnitude() - self.world.character.hit_radius < self.hit_radius:
+#             self.world.gameover = True
 
 
 
--- Walls.py
+++ Walls.py
@@ -2,34 +2,18 @@ from geometry import Point2D, Vector2D
 
 class Wall:
 
-    def __init__(self,TOP_LEFT,BOTTOM_RIGHT,world):
-        self.TOP_LEFT = TOP_LEFT
-        self.BOTTOM_RIGHT = BOTTOM_RIGHT
-        self.world = world
-        self.shapekind = 'poly'
-        self.world.add(self)
+    def __init__(self,width):
+        side = float(self.bounds.xmax)
+        top = float(self.bounds.ymax)
+        wide = float(self.wallwidth)
 
-    def shape(self):
-        p1 = self.TOP_LEFT
-        p2 = Point2D(self.BOTTOM_RIGHT.x,self.TOP_LEFT.y)
-        p3 = self.BOTTOM_RIGHT
-        p4 = Point2D(self.TOP_LEFT.x,self.BOTTOM_RIGHT.y)
-        return[p1,p2,p3,p4]
 
-    def color(self):
-        return '#FFFFFF'
-    
-    def update(self):
-        pass
 
-class Walls:
+        
+    def pointsGen(self,topleft,botright):
+        p1 = topleft
+        p2 = Point2D(botright.x,topleft.y)
+        p3 = botright
+        p4 = topleft[0],botright[1]
+        return [p1,p2,p3,p4]
 
-    def __init__(self,width,world):
-        self.width = width
-        self.contain = []
-        self.world = world
-
-        self.contain.append(Wall(Point2D(-30.0,22.5),Point2D(31.0,22.5-self.width),self.world))
-        self.contain.append(Wall(Point2D(30.0-self.width,22.5-self.width),Point2D(31.0,-22.5+self.width),self.world))
-        self.contain.append(Wall(Point2D(-30.0,-22.5+self.width),Point2D(31.0,-23.5),self.world))
-        self.contain.append(Wall(Point2D(-30.0,22.5-self.width),Point2D(-30.0+self.width,-22.5+self.width),self.world))
